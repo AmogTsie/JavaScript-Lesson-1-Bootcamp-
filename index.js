ce98eea40022125318502178732d3c9f5bff7192
AmogTsie <amogtsie@gmail.com>
2024-04-17 10:35:55 +0200

    Arrays + Object

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-//intro to function:
+//Arrays of students 
+let listOFStudents = [ "Amo", "Bod", "Kyle"]
+console.log(listOfStudents);
+console.log(listOFStudents.length);
 
-//function decleration
-function count(){
-    //function declaration
-   console.log("hi");
-}
-count()  //this is how you call a function
+//add an Item to arry
+listOFStudents.push("Bob")
+console.log( listOFStudent);
 
-const count = function( ) {
-    //function expression
-    //cant call it before declartion
-    console.log("hit there " + user );
+//remove an item from an array
+listOFStudents.splice(0, 1)
+console.log(listOFStudents);
+
+for(let i = 0; i <listOFStudents.length;i++) {
+    console.log("for loop: ", listOFStudents)
 }
-countToo("Tsie")
+
+// working with objects 
+
+let student = {
+    name: "Amo",
+    surname: "Tsie",
+    age: 22,
+    isFacilitator: true
+}
+
+console,log( student );
+
+// edit values in an object
+student.name = "Amo"
+console.log("after editing: ", student);
+
+//adding values in an object
+
+student.isPrison = false;
+student["isInSchool"] = false;
+delete student.age // this removes 
+
+console.log("after adding: ", student);
