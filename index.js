9f98cf87f1bc014b485657215236cca154aab88c
AmogTsie <amogtsie@gmail.com>
2024-04-17 10:33:48 +0200

    Functions

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
-//Intro to operators:
-//mathematical opperators (division(/), subtraction(-), addition(+),multiplication(*))
+//intro to function:
 
-//string concatenator
-console.log("hi there" +  "my name is Amo");
-
-//number addition
-console.log( 3+5);
-
-//conversion: explicit conversion ny the user
-//coercion: by the language itself(JavaScript)
-
-console.log(String(3) + String(5));
-
-//direct conversions:
-console.log("5" + "9");
-console.log(Number("5") + Number("9"));
-
-//implicit converison:
-console.log( 5 + 10 + "4");
-
-//subtract
-let firstNumber = 100;
-let secondNumber = 50;
-
-console.log(firstNumber - secondNumber);
-
-//division
-console.log( firstNumber/ secondNumber);
-
-//multiplicaion
-console.log(firstNumber * secondNumber);
-
-//logical opperators (&&, ||, ==, ===, !==):
-
-//if statements: comtrols how opperation is running
-
-if(5 === 5) {
-   console.log(true);
+//function decleration
+function count(){
+    //function declaration
+   console.log("hi");
 }
-else{
-   console.log(false);
-}
+count()  //this is how you call a function
 
+const count = function( ) {
+    //function expression
+    //cant call it before declartion
+    console.log("hit there " + user );
+}
+countToo("Tsie")
